8102eeea4624b85bd00fe30cec2d7ccc3e14f029
liujunyi <liujunyi_coder@163.com>
2024-12-02 14:37:59 +0800

    日志改版

--- zaplog/channel/file.go
+++ zaplog/channel/file.go
@@ -8,11 +8,6 @@ import (
 // NewFileChannel 注册文件通道
 func NewFileChannel(conf *core.Config) zapcore.Core {
 	config := core.NewEncoderConfig(false)
-	var encoder zapcore.Encoder
-	if conf.Debug {
-		encoder = zapcore.NewConsoleEncoder(config)
-	} else {
-		encoder = zapcore.NewJSONEncoder(config)
-	}
+	encoder := zapcore.NewJSONEncoder(config)
 	return zapcore.NewCore(encoder, zapcore.AddSync(conf.FileConf.Logger), conf.FileConf.Level)
 }
